aacf0fd162efa1bb0bc43d2caa3f6bc82e252072
Cristopher Josue Herrera Marquina <123046753@upq.edu.mx>
2026-05-19 15:29:03 -0600

    Caja terminada y cliente a la mitad

---
+++ CajaLogica.js
@@ -0,0 +1,36 @@
+const productos = [];
+// Agregar productos al menú
+
+productos.push({ nombre: "Cafe Americano", precio: 10 });
+productos.push({ nombre: "Cafe con Leche", precio: 12 });
+productos.push({ nombre: "Cafe Capuchino", precio: 15 });
+productos.push({ nombre: "Cafe Expreso", precio: 8 });
+// Mostrar el menú de productos
+console.table(productos);
+// Función para agregar un pedido
+const pedidos = [];
+
+function agregarPedido(nombre, precio) {
+    const pedido = { nombre, precio };
+    pedidos.push(pedido);
+    console.log("Pedido agregado: ", pedido);
+}
+// Función para calcular el total acumulado de los pedidos
+const totalAcumulado = () => {
+    let total = 0;
+    pedidos.forEach(pedido => {
+        total += pedido.precio;
+    });
+    return total;
+};
+// Función para mostrar el total acumulado
+function mostrarTotal() {
+    const total = totalAcumulado();
+    console.log("Total acumulado: ", total);
+}
+// Exportar las funciones para su uso en otros archivos
+
+// Ejemplo de uso
+agregarPedido("Cafe Americano", 10);
+agregarPedido("Cafe con Leche", 12);
+mostrarTotal();
