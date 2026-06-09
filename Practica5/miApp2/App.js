1f2c197f1e8c1cd82a32889a63da147f3f79cf5e
Cristopher Josue Herrera Marquina <123046753@upq.edu.mx>
2026-06-09 14:42:41 -0600

    TAREA P6> Component perfil

--- Practica5/miApp2/App.js
+++ Practica5/miApp2/App.js
@@ -1,27 +1,28 @@
-
-//ZONA1> Importacion de archivos y componentes
+//Zona 1: importación de archivos y componentes.
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import {Saludo} from './components/Saludo';
-import {Saludo2} from './components/Saludo2';
-//ZONA2> Main -Componentes
+import { StyleSheet, Text, View, Image} from 'react-native';
+import{Saludo} from './components/Saludo';
+import{Saludo2} from './components/Saludo2';
+import{Perfil} from './components/Perfil';
+
+//Zona2: Componentes
 export default function App() {
-  return (  
-
+  return (
     <View style={styles.container}>
-      <Image source={require('./assets/wave.png')} />
-      <Text>-----------------------------------</Text>
+      <Image source ={require('./assets/wave.png')}/>
+      <Text>Hola Mundo!</Text>
+      <Text>---------------------</Text>
       <Saludo/>
-      <Text>-----------------------------------</Text>
+      <Text>---------------------</Text>
       <Saludo2/>
-      <Text>Hola Mundo React Native</Text>
+      <Text>---------------------</Text>
+      <Perfil/>
       <StatusBar style="auto" />
-
     </View>
-
   );
 }
-//ZONA3> Estilos
+
+//Zona3: Estilos y posicionamiento
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ Practica5/miApp2/components/Perfil.js
@@ -0,0 +1,15 @@
+import {View, Text} from 'react-native';
+
+
+//La funcion de flecha omra de simplificar funciones
+export const Perfil=() =>{
+    return(
+        <View>
+            <Text>Repaso de Componentes{'\n'} </Text>
+            <Text>Nombre : Cristopher Josue Herrera Marquina{'\n'} </Text>
+            <Text>Carrera : Ingeniería en Sistemas Computacionales {'\n'} </Text>
+            <Text>Materia : Programación Móvil{'\n'} </Text>
+            <Text>Cuatrimestre : 9° cuatrimestre {'\n'}</Text>
+        </View>
+    )
+}
